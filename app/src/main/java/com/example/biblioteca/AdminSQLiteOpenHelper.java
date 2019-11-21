63acc1369b7a5af95e578a2998a901c394da1159
jose <jose.tello@gmail.com>
2019-11-21 09:41:23 -0300

    Se agrega funciones y configuracion de base de datos

--- app/src/main/java/com/example/biblioteca/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/biblioteca/AdminSQLiteOpenHelper.java
@@ -1,4 +1,24 @@
 package com.example.biblioteca;
 
-public class AdminSQLiteOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase; // Importamos clase base de datos.
+import android.database.sqlite.SQLiteOpenHelper; // Importamos clase OpenHelper.
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase Database) {
+        // Creando nuestra primera tabla con SQLite.
+        Database.execSQL("CREATE TABLE libros(codigo int primary key, nombre text, precio float)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
