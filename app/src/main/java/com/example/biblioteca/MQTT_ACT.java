fb07f8c8e0b58f20f80372d512457324396b1c7f
jose <jose.tello@gmail.com>
2019-12-11 10:11:14 -0300

    Se crean dos activity para mensajeria mqtt

---
+++ app/src/main/java/com/example/biblioteca/MQTT_ACT.java
@@ -0,0 +1,14 @@
+package com.example.biblioteca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MQTT_ACT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mqtt__act);
+    }
+}
