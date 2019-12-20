60c49cd3852ce0daa1369e04f1b2f9d1212ac482
Omar Perez <52640240+makana20@users.noreply.github.com>
2019-12-19 22:31:40 -0300

    Datos

--- app/src/main/java/com/example/biblioteca/Security_ACT.java
+++ app/src/main/java/com/example/biblioteca/Security_ACT.java
@@ -18,49 +18,28 @@ public class Security_ACT extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security__act);
 
-        tx = (TextView)findViewById(R.id.text_tipo_vul);
-        btn_vul1 = (Button)findViewById(R.id.btn_vul1);
-        btn_vul2 = (Button)findViewById(R.id.btn_vul2);
-        btn_vul3 = (Button)findViewById(R.id.btn_vul3);
+        tx = (TextView) findViewById(R.id.text_tipo_vul);
+        btn_vul1 = (Button) findViewById(R.id.btn_vul1);
+        btn_vul2 = (Button) findViewById(R.id.btn_vul2);
+        btn_vul3 = (Button) findViewById(R.id.btn_vul3);
     }
 
-    public void vulnerabilidad_1(View view){
-        String username = "jose";
-        String password = "1234asd";
+    public void vulnerabilidad_1(View view) {
+        String username = "Andres";
+        String password = "1234asl";
 
-        // Las variables nunca deben ser visibles si es posible estas deben se encriptadas para
-        // evitar su manipulación y desemcriptadas para su utilizacion dentro de un programa.
         msg = "Encriptación de datos:\n" + username + "\n" + password;
 
         tx.setText(msg);
     }
 
-    public void vulnerabilidad_2(View view){
+    public void vulnerabilidad_2(View view) {
         ContentValues datos = new ContentValues();
 
-        // Agregar datos sin especificar su tamaño y confirmacion de estado es una vulnerabilidad
-        // importante ya que esta puede probocar desbordamiento de memoria y abrir una puerta para
-        // vulnerar la seguridad de esta.
-        datos.put("nombre", "carlos");
-        datos.put("edad", "23");
+        datos.put("Andres", "Rolando");
+        datos.put("edad", "25");
 
-        msg = "Desbordamiento de Memoria";
-
-        tx.setText(msg);
-    }
-
-    public void vulnerabilidad_3(View view){
-        String url = "https://www.google.cl";
-        String user = "Makana";
-        String address = "Santiasco";
-        String query = "insert into user where name = " + user;
-
-        // El no utilizar metodos de conexion y validacion de datos para la insercion de estos a
-        // una base de datos es una vulnerabilidad grave ja que puede ser sujeta a una inserción
-        // de sql, el utilizar los metodos proporcionados por los frame o api de conexion nos,
-        // permitiran validar los datos de entrada y tambien evitar realizar ingreso de query de
-        // forma manual las que pueden ser manipuladas si son vulneradas.
-        msg = "Injeccion de SQL";
+        msg = "Desbr de Memoria";
 
         tx.setText(msg);
     }
